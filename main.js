ef3b7979f71ad7dac8c82aeb97f9378627efc004
Virgarakha <142412561+Virgarakha@users.noreply.github.com>
2023-12-11 21:41:39 +0700

    main.js

--- main.js
+++ main.js
@@ -3,93 +3,32 @@ function kirimData() {
     const password = document.getElementById("password").value;
 
 
-    const user = ("virgarakha@rpl.co.id");
+    const user = ("rakha@gmail.com");
     const pass = 9913;
+    
+    
 
-    if (username == "" ||  password == ""){
+    if (user == "" ||  password == ""){
+        alert("harap isi username dan pasword")
     }else if (password != pass ) {
  
-    }else if (username != user ) {
+    }else if (username != user) {
  
     }else {
-     alert ("Log Berhasil",document.getElementById("rakha").click());
+        alert("Login Berhasil" , document.getElementById("rakha").click());
     }
+    const user2 = ("anas@gmail.com");
+    const pass2 = 9979;
     
-    const user1 = ("anasmaulana@rpl.co.id");
-    const pass1 = 9989;
+    
 
-    if (username == "" ||  password == ""){
-    }else if (password != pass1 ) {
- 
-    }else if (username != user1 ) {
- 
-    }else {
-    document.getElementById("anas").click();
-    }
-    const user2 = ("ryalfitrah@rpl.co.id");
-    const pass2 = 9927;
-
-    if (username == "" ||  password == ""){
+    if (user == "" ||  password == ""){
+        alert("harap isi username dan pasword")
     }else if (password != pass2 ) {
  
     }else if (username != user2 ) {
  
     }else {
-    document.getElementById("ryal").click();
+        alert("Login Berhasil" , document.getElementById("anas").click());
     }
-    const user3 = ("adithdwi@rpl.co.id");
-    const pass3 = 9920;
-
-    if (username == "" ||  password == ""){
-    }else if (password != pass3 ) {
- 
-    }else if (username != user3 ) {
- 
-    }else {
-    document.getElementById("adith").click();
-    }
-    const user4 = ("fataroriza@rpl.co.id");
-    const pass4 = 9979;
-
-    if (username == "" ||  password == ""){
-    }else if (password != pass4 ) {
- 
-    }else if (username != user4 ) {
- 
-    }else {
-    document.getElementById("fatar").click();
-    }
-    const user5 = ("aditmaulana@rpl.co.id");
-    const pass5 = 9910;
-
-    if (username == "" ||  password == ""){
-    }else if (password != pass5 ) {
- 
-    }else if (username != user5 ) {
- 
-    }else {
-    document.getElementById("adit").click();
-    }
-    const user6 = ("giovani@rpl.co.id");
-    const pass6 = 9911;
-
-    if (username == "" ||  password == ""){
-    }else if (password != pass6 ) {
- 
-    }else if (username != user6 ) {
- 
-    }else {
-    document.getElementById("gio").click();
-    }
-    const user7 = ("fareltrio@rpl.co.id");
-    const pass7 = 9917;
-
-    if (username == "" ||  password == ""){
-    }else if (password != pass7 ) {
- 
-    }else if (username != user7 ) {
- 
-    }else {
-    document.getElementById("farel").click();
-    }
-}
+} 
